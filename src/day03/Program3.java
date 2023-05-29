cda9b1af980785d052793fc07c1300efa6951d02
sowmiya <129870504+sowmiya93@users.noreply.github.com>
2023-05-29 21:02:00 +0530

    code for xpath

--- src/day03/Program3.java
+++ src/day03/Program3.java
@@ -1,5 +1,21 @@
 package day03;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class Program3 {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+		driver.findElement(By.xpath("//span[@aria-labelledby='select2-country-container']")).click();
+		driver.findElement(By.xpath("//input[@class='select2-search__field']")).sendKeys("India");
+		driver.findElement(By.xpath("//li[@class='select2-results_option select2-results_option--highlighted']")).click();
+		
+		
+	}
 
 }
